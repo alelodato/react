07bd02a16f2ccc5c7cdc70319fa773676243bcde
Alessio Lodato <swly7j4e@students.codeinstitute.net>
2025-02-20 00:01:56 +0000

    created stateful component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps name="Alessio!" age="I can see you're 30 years old;" greeting="nice to meet you!!" />
+      <StatefulGreeting name="Alessio," greeting="I'm a stateful class component!" />
     </div>
   );
 }
