3d5bd92d3a3daca9336dc9ebf00ba8e8d70b0863
Alessio Lodato <swly7j4e@students.codeinstitute.net>
2025-02-20 17:03:55 +0000

    completed navbar challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
+import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
+import MethodsAsPropsChild from './components/MethodsAsPropsChild';
 
 function App() {
-  return (
-    <div className="App">
-      <StatefulGreetingWithPrevState />
-    </div>
-  );
+  return <div className="App">
+      <MethodsAsPropsParent />
+  </div>;
 }
 
 export default App;
